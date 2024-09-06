f5f11110979e3a80fd5ac6fcf49032505238bd95
judysongg <judysongg@gmail.com>
2024-09-05 19:09:21 -0700

    testing

--- client/components/RecipeList.js
+++ client/components/RecipeList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Recipe from './Recipe';
 export const RecipeList = ({ recipeList, handleEdit, handleDelete }) => {
+  console.log('recipeList', recipeList)
     
     return (
         <div className="flex flex-col">
             {recipeList.map(recipe => 
                 <Recipe handleDelete={handleDelete} handleEdit={handleEdit} key={recipe.recipe_id }id={recipe.recipe_id} title={recipe.title} description={recipe.description}/>
             )}
+            c
         </div>
     )
 }
